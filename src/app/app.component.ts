9af04596ec4640bd3afeb86b33b4d1b1649611b7
Nicolas Perrin <nicolas@perrinn.com>
2024-02-25 18:07:48 +0000

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,9 @@ import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/comp
     </div>
     <div style="user-select:none">
       <div *ngIf="UI.currentUser" style="max-width:800px;margin:0 auto">
+        <span class="material-icons" style="float:right;margin:5px;cursor:pointer;color:black" (click)="router.navigate(['apps'])">apps</span>
+        <div style="float:right;width:1px;height:35px;cursor:pointer;border-style:solid;border-width:0 1px 0 0;border-color:#ddd"></div>
+        <span class="material-icons" style="float:right;margin:5px;cursor:pointer;color:black" (click)="newMessage()">create</span>
         <div style="float:left;width:180px;height:35px;cursor:pointer" (click)="router.navigate(['profile',UI.currentUser])">
           <img *ngIf="UI.currentUserLastMessageObj?.imageUrlThumbUser" [src]="UI.currentUserLastMessageObj.imageUrlThumbUser" style="display:inline;float:left;margin:4px;border-radius:50%;object-fit:cover;width:25px;height:25px">
           <div style="float:left;margin:8px;font-size:12px">
@@ -24,11 +27,10 @@ import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/comp
             <span style="margin-left:10px"> {{UI.appSettingsCosts?.interestRateYear | percent : "0.0"}} APY</span>
           </div>
         </div>
-        <div style="float:left;font-size:12px;text-align:center;line-height:15px;width:65px;padding:2px;margin:7px;color:white;background-color:black;border-radius:3px;cursor:pointer" (click)="router.navigate(['buy'])">Buy</div>
-        <div style="float:left;font-size:12px;text-align:center;line-height:15px;width:65px;padding:2px;margin:7px;color:black;border-radius:3px;cursor:pointer;border-width:1px;border-style:solid" (click)="router.navigate(['sell'])">Sell</div>
-        <span class="material-icons" style="float:right;margin:5px;cursor:pointer;color:black" (click)="router.navigate(['apps'])">apps</span>
-        <div style="float:right;width:1px;height:35px;cursor:pointer;border-style:solid;border-width:0 1px 0 0;border-color:#ddd"></div>
-        <span class="material-icons" style="float:right;margin:5px;cursor:pointer;color:black" (click)="newMessage()">create</span>
+        <div style="float:left;width:180px">
+          <div style="float:left;font-size:12px;text-align:center;line-height:15px;width:65px;padding:2px;margin:7px;color:white;background-color:black;border-radius:3px;cursor:pointer" (click)="router.navigate(['buy'])">Buy</div>
+          <div style="float:left;font-size:12px;text-align:center;line-height:15px;width:65px;padding:2px;margin:7px;color:black;border-radius:3px;cursor:pointer;border-width:1px;border-style:solid" (click)="router.navigate(['sell'])">Sell</div>
+        </div>
       </div>
       <div class="seperator" style="width:100%;margin:0px"></div>
     </div>
